67e7a6da8910db679e5d98e9b96ef5046056e6dd
Jammie623 <71751181+Jammie623@users.noreply.github.com>
2020-11-18 14:32:25 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
 })
 radio.setGroup(5)
